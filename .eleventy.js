e29488c84a56362de5d30f9218096dec4a2a8096
Hassan Andrabi <handrabi53@gmail.com>
2021-08-27 08:53:59 +1000

    Added excel model for TB piece upload

--- .eleventy.js
+++ .eleventy.js
@@ -40,6 +40,7 @@ module.exports = function(eleventyConfig){
 
 
 
+	eleventyConfig.addPassthroughCopy("./src/assets");
 	eleventyConfig.addPassthroughCopy("./src/admin");
     return {
         dir: {
